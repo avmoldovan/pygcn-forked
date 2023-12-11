a108948ef5d8a5c7e47cd4194f2569b98f438e4d
Adrian Moldovan <adrian.moldovan@gmail.com>
2023-12-12 00:13:25 +0200

    adding CORA sample from torch geometric

--- pygcn/train.py
+++ pygcn/train.py
@@ -18,7 +18,7 @@ from PyIF import te_compute as te
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='Disables CUDA training.')
-parser.add_argument('--fastmode', action='store_true', default=False,
+parser.add_argument('--fastmode', action='store_true', default=True,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=200,
